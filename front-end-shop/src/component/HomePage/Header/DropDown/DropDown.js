b00cb66d6924f45881d7ad30e251ac7654aa509c
ndai2010 <dangdai2010@gmail.com>
2022-02-20 16:59:10 +0700

    dropdown

---
+++ front-end-shop/src/component/HomePage/Header/DropDown/DropDown.js
@@ -0,0 +1,91 @@
+import React from "react";
+class DropDown extends React.Component {
+    render() {
+        return (
+            <div className="navigation">
+                <ul className="parent-dropdown-container">
+                    <li className="parent-dropdown">
+                        <div className="title">
+                            <span>Balo</span>
+                            <i className="fa-solid fa-caret-down"></i>
+                        </div>
+                        <ul className="dropdown-menu">
+                            <li className="dropdow-content">
+                                <ul>
+                                    <li>phụ kiện balo</li>
+                                    <li>balo theo loại</li>
+                                    <li>balo theo thương hiệu</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="parent-dropdown">
+                        <div className="title">
+                            <span>Trang phục</span>
+                            <i className="fa-solid fa-caret-down"></i>
+                        </div>
+                        <ul className="dropdown-menu">
+                            <li className="dropdow-content">
+                                <ul>
+                                    <li>phụ kiện balo</li>
+                                    <li>balo theo loại</li>
+                                    <li>balo theo thương hiệu</li>
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </li>
+                    <li className="parent-dropdown">
+                        <div className="title">
+                            <span>Dao - dụng cụ</span>
+                            <i className="fa-solid fa-caret-down"></i>
+                        </div>
+                        <ul className="dropdown-menu">
+                            <li className="dropdow-content">
+                                <ul>
+                                    <li>phụ kiện balo</li>
+                                    <li>balo theo loại</li>
+                                    <li>balo theo thương hiệu</li>
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </li>
+                    <li className="parent-dropdown">
+                        <div className="title">
+                            <span>Đồ dã ngoại</span>
+                            <i className="fa-solid fa-caret-down"></i>
+                        </div>
+                        <ul className="dropdown-menu">
+                            <li className="dropdow-content">
+                                <ul>
+                                    <li>phụ kiện balo</li>
+                                    <li>balo theo loại</li>
+                                    <li>balo theo thương hiệu</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="parent-dropdown">
+                        <div className="title">
+                            <span>Phụ kiện</span>
+                            <i className="fa-solid fa-caret-down"></i>
+                        </div>
+                        <ul className="dropdown-menu">
+                            <li className="dropdow-content">
+                                <ul>
+                                    <li>phụ kiện balo</li>
+                                    <li>balo theo loại</li>
+                                    <li>balo theo thương hiệu</li>
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default DropDown
--- front-end-shop/src/component/HomePage/Header/HomeHeader.js
+++ front-end-shop/src/component/HomePage/Header/HomeHeader.js
@@ -1,17 +1,20 @@
 import React from "react";
 import './HomeHeader.scss'
+import DropDown from "./DropDown/DropDown";
 class HomeHeader extends React.Component {
     render() {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="content">
+                    <div className="content-header">
                         <div className="left-content"></div>
-                        <div className="between-content"></div>
+                        <div className="between-content">
+                            <DropDown />
+                        </div>
                         <div className="right-content"></div>
                     </div>
                 </div>
-            </div>
+            </div >
         )
     }
 }
